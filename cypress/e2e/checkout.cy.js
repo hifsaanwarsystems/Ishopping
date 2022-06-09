d804f91f62a08a546f71d50e3ff868cf2282d21f
hifsaanwarsystems <hifsa.anwar@systemsltd.com>
2022-06-09 15:37:21 +0500

    shipping issue

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,6 +1,30 @@
+import { Customer } from './customer.cy'
+const customerAccountCreateDetails=require('../fixtures/customer.createaccount.json')
+
 export class Checkout{
    addproducts()
    {
-     
+     cy.get(':nth-child(3) > .has-sub-cat').click()
+     cy.get('.categories-holder > [href="https://ishopdev.ddways.com/women/tops-women.html"]').click()
+     cy.get('#product-item-info_2087 > .product-item-details > .bottom-bar > form > .icon-rounded').click()
+     cy.get('.message-success').should('be.visible')
+   }
+   openingCart()
+   {
+      cy.get('.minicart-wrapper').click()
+      cy.wait(10000)
+      cy.get('#top-cart-btn-checkout').click()
+   }
+   signinIncustomer()
+   {
+    cy.wait(10000)
+     cy.get('#checkout-step-title > .actions-toolbar > .secondary > .action > span').click()
+     let customerSignin=new Customer()
+     cy.wait(10000)
+     customerSignin.emailLogin(customerAccountCreateDetails.email,customerAccountCreateDetails.password)
+   }
+   shippingInfo()
+   {
+     cy.get('#F3DC9B3').click()
    }
 }
--- cypress/e2e/main.checkout.cy.js
+++ cypress/e2e/main.checkout.cy.js
@@ -6,6 +6,12 @@ const customerAccountCreateDetails=require('../fixtures/customer.createaccount.j
 describe('Ishopping Tests Customer Flow', () => {
     it('Checkout flow',function()
     {
+        let checkout=new Checkout()
         cy.visit('https://ishopdev.ddways.com/',{failOnStatusCode:false,auth:{username:"developers",password:"xN9PHjF9cM4DRXN2"}})
+        checkout.addproducts()
+        checkout.openingCart()
+        checkout.signinIncustomer()
+        checkout.shippingInfo()
+
     })
 })
